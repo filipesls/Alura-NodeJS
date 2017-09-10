51dc3611d680d2a2a53e0af7877381c18642dc74
Filipe Santana <filipe.santana@gmail.com>
2017-09-10 22:24:12 +0100

    Aula 2.16

--- projeto/casadocodigo/app/routes/produtos.js
+++ projeto/casadocodigo/app/routes/produtos.js
@@ -1,9 +1,10 @@
 module.exports = function(app) {
-    app.get("/produtos",function(req, res) {
+    app.get('/produtos', function(req, res) {
 
         var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco;
 
-        connection.query('select * from produtos', function(err, results){
+        produtosBanco.lista(connection, function(err, results){
             res.render('produtos/lista', {lista: results});
         });
 
